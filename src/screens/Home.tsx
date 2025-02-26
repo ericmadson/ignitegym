47e3299e98f2cd64fe73a627d09015ef5a3cf5e0
Eric Madson <ericmadson67@hotmail.com>
2025-02-25 22:02:50 -0300

    feat: home exercise group selector

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,49 @@
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "Ombros",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8">
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
