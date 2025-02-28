58029a41dfc2d8c1b27d357061a877709383ed42
Eric Madson <ericmadson67@hotmail.com>
2025-02-28 10:22:45 -0300

    feat: profile photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,33 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/ericmadson.png" }}
+            alt="Foto do usuário"
+            size="xl"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
